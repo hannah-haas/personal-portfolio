293a5f8374b2b9176440424809c6edea82483c16
Hannah Haas <hannahhaas@Hannahs-MacBook-Pro-4.local>
2020-08-27 11:51:42 -0400

    Updates

---
+++ precache-manifest.088d64a2f28c175ecdd678fd9009f729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d69a73475a4f5e83b668e79ac0ca75",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "c06a1a0de869e8c43b0c",
+    "url": "/personal-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b760aed8ab22ba3f12f",
+    "url": "/personal-portfolio/static/js/2.97209924.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-portfolio/static/js/2.97209924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06a1a0de869e8c43b0c",
+    "url": "/personal-portfolio/static/js/main.986e6396.chunk.js"
+  },
+  {
+    "revision": "2cecf7e1847107db1f4e",
+    "url": "/personal-portfolio/static/js/runtime-main.f2f5ea2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
